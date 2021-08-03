79d2020f0f73f9c8ee8722266cafc16a48d72c00
91991 <91991@LAPTOP-1MA5CO24>
2021-08-03 15:28:20 +0530

    Fourth commite

--- src/main/java/com/git/test/MessageRunner.java
+++ src/main/java/com/git/test/MessageRunner.java
@@ -1,15 +1,24 @@
 package com.git.test;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MessageRunner implements CommandLineRunner {
 
+	private static Logger log = LoggerFactory.getLogger(MessageRunner.class);
 	public void run(String... args) throws Exception {
+		try {
+			log.info("STARTED");
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
 		System.out.println("GIT Application");
 		int a= 10;
 		System.out.println("Modifivation done :: "+a);
+		log.info("STARTED");
 	}
 
 }
